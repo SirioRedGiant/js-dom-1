76bea2c4a5fb23d6ee6287835273ae4679aada40
SirioRedGiant <Oiris1992@gmail.com>
2026-01-12 14:45:18 +0100

    debugging

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const lampadinaSpenta = document.getElementById("turned_off_lamp");
 const interruttoreLuce = document.getElementById("interruttore");
 
 //todo usare w3school per trovare le proprietà e i metodi: link => https://www.w3schools.com/jsref/dom_obj_document.asp
+console.log(lampadinaSpenta);
 
 // Funzione che data un azione(CLICK) cambia l'immagine della lampadina spenta a quella accesa
 interruttoreLuce.addEventListener("click", () => {
   lampadinaSpenta.src = "./assets/img/yellow_lamp.png";
+  console.log(lampadinaSpenta);
 });
+console.log(lampadinaSpenta);
